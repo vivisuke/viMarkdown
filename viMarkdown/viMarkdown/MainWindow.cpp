a3573f5c1057d1c49b0b48656014522a14f1ab6b
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2025-12-13 09:07:19 +0900

    0001 ◎ C 12/13 12/13 0.001│ToDo│プロジェクト作成

---
+++ viMarkdown/viMarkdown/MainWindow.cpp
@@ -0,0 +1,14 @@
+#include "MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindowClass())
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
