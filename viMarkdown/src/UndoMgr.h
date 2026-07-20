4f291c4089a98df42164eb623cec1cdec2374eef
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-07-20 13:12:34 +0900

    0673 ◎ C 07/20 07/20 03037│ToDo│自前の UndoManegaer 導入（UndoMgr.h, .cpp）

--- viMarkdown/src/UndoMgr.h
+++ viMarkdown/src/UndoMgr.h
@@ -1,4 +1,32 @@
-#pragma once
+﻿#pragma once
+
+using uchar = unsigned char;
+
+struct UndoAction
+{
+	enum {
+		ACT_UNDEF = 0,
+		ACT_INSERT,
+		ACT_DELETE,
+		ACT_REPLACE,
+		//ACT_REPLACE_ALL,
+
+		FLAG_BLOCK = 1,		//	このビットが立っていれば、undo block 開始 or 終了
+		//FLAG_MODIFIED = 2,	//	モディファイフラグ
+		FLAG_BS = 2,		//	BackSpace による削除
+	};
+	uchar	m_type;
+	uchar	m_flags;
+
+public:
+	UndoAction(uchar type = 0, uchar flags = 0)
+		: m_type(type)
+		, m_flags(flags)
+		{}
+	virtual ~UndoAction() {};
+};
+
 class UndoMgr
-{};
+{
+};
 
