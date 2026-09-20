021180d2de34d208b35ad9c76210ac61b8406b11
Nobuhide Tsuda <ntsuda@master.email.ne.jp>
2026-09-21 06:14:44 +0900

    1087 ◎ C 09/21 09/21 03113│問題│チートシート > 「:{r}n[ext]     next file」は0.5以降に延期したので削除した方がよい

--- viMarkdown/src/MainWindow.cpp
+++ viMarkdown/src/MainWindow.cpp
@@ -1714,9 +1714,9 @@ EX
   :w[rite]  save                          :q[uit]/!   quit / force quit
   :{n}      jump to line n                :{r}d[elete] delete range
   :{r}p     print lines                   :{r}P       print with line nums
-  :{r}s/pat/rep/[g]  substitute           :{r}g/pat/cmd  global execution *
-  :{r}nu[mber]       number lines         :{r}n[ext]     next file *
-  :w filename        save as *            Up/Down     command history  
+  :{r}s/pat/rep/[g]  substitute           :{r}g/pat/cmd  global execution
+  :{r}nu[mber]       number lines         :w filename        save as
+  Up/Down     command history  
  
   Range {r} modifiers:
     {num}   line num         .  current line     $  last line
